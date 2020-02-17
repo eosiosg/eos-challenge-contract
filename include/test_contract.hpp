4c0a4b887b415817bb4d52c64d726f4e8c4f2f13
Frank-AFN <a870066721@gmail.com>
2020-02-17 19:34:26 +0800

    Add: check

--- include/test_contract.hpp
+++ include/test_contract.hpp
@@ -7,6 +7,7 @@ CONTRACT test_contract : public contract {
       using contract::contract;
 
       ACTION hi( name nm );
+      ACTION check(eosio::checksum256 &hash, const uint8_t version, const eosio::checksum256 r, const eosio::checksum256 s)
       void ecrecover2(const evmc_uint256be hash, const uint8_t version, const evmc_uint256be r, const evmc_uint256be s);
 
       using hi_action = action_wrapper<"hi"_n, &test_contract::hi>;
--- src/test_contract.cpp
+++ src/test_contract.cpp
@@ -8,6 +8,11 @@ ACTION test_contract::hi( name nm ) {
    print_f("Name : %\n",nm);
 }
 
+ACTION test_contract::check(eosio::checksum256 &hash, const uint8_t version, const eosio::checksum256 r, const eosio::checksum256 s) {
+  hash.print();
+  //evmc_address res = ecrecover(hash, version, r, s);
+}
+
 void test_contract::ecrecover2(const evmc_uint256be hash, const uint8_t version, const evmc_uint256be r, const evmc_uint256be s){
   std::vector<uint8_t> signature;
   std::copy(r.bytes, r.bytes + sizeof(evmc_uint256be),
@@ -16,31 +21,31 @@ void test_contract::ecrecover2(const evmc_uint256be hash, const uint8_t version,
 			std::back_inserter(signature));
 
   static secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
-//  secp256k1_ecdsa_recoverable_signature ecsig;
-//  if (!secp256k1_ecdsa_recoverable_signature_parse_compact(
-//	  ctx, &ecsig, (unsigned char *) &signature[0], version)) {
-//	return zero_address;
-//  }
-//
-//  secp256k1_pubkey ecpubkey;
-//  if (!secp256k1_ecdsa_recover(ctx, &ecpubkey, &ecsig, hash.bytes)) {
-//	return zero_address;
-//  }
-//  size_t pubkeysize = 65;
-//  unsigned char pubkey[65];
-//  secp256k1_ec_pubkey_serialize(ctx, pubkey, &pubkeysize, &ecpubkey,
-//								SECP256K1_EC_UNCOMPRESSED);
-//
-//  assert(pubkey[0] == 4);
-//  assert(pubkeysize == 65);
-//  assert(pubkeysize > 1);
-//  // skip the version byte at [0]
-//  auto pubkeyhash =
-//	  ethash::keccak256((uint8_t * )(pubkey + 1), pubkeysize - 1);
-//
-//  evmc_address address;
-//  std::copy(pubkeyhash.bytes + (sizeof(evmc_uint256be) - sizeof(evmc_address)),
-//			pubkeyhash.bytes + sizeof(evmc_uint256be), address.bytes);
-//
-//  return address;
+  secp256k1_ecdsa_recoverable_signature ecsig;
+  if (!secp256k1_ecdsa_recoverable_signature_parse_compact(
+	  ctx, &ecsig, (unsigned char *) &signature[0], version)) {
+	return zero_address;
+  }
+
+  secp256k1_pubkey ecpubkey;
+  if (!secp256k1_ecdsa_recover(ctx, &ecpubkey, &ecsig, hash.bytes)) {
+	return zero_address;
+  }
+  size_t pubkeysize = 65;
+  unsigned char pubkey[65];
+  secp256k1_ec_pubkey_serialize(ctx, pubkey, &pubkeysize, &ecpubkey,
+								SECP256K1_EC_UNCOMPRESSED);
+
+  assert(pubkey[0] == 4);
+  assert(pubkeysize == 65);
+  assert(pubkeysize > 1);
+  // skip the version byte at [0]
+  auto pubkeyhash =
+	  ethash::keccak256((uint8_t * )(pubkey + 1), pubkeysize - 1);
+
+  evmc_address address;
+  std::copy(pubkeyhash.bytes + (sizeof(evmc_uint256be) - sizeof(evmc_address)),
+			pubkeyhash.bytes + sizeof(evmc_uint256be), address.bytes);
+
+  return address;
 }
