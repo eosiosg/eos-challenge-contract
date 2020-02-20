60e430dc13a37c6a9cdb7a4e1fb4a588fd7871a0
Frank-AFN <a870066721@gmail.com>
2020-02-20 13:11:02 +0000

    v0.1.0 work secp256k1-eosio

--- include/test_contract.hpp
+++ include/test_contract.hpp
@@ -9,6 +9,7 @@ CONTRACT test_contract : public contract {
       ACTION hi( name nm );
       ACTION check(eosio::checksum256 &hash, const uint8_t version, const eosio::checksum256 r, const eosio::checksum256 s);
       evmc_address ecrecover2(const evmc_uint256be hash, const uint8_t version, const evmc_uint256be r, const evmc_uint256be s);
+      void assert_b(bool test, const char *msg);
 
       using hi_action = action_wrapper<"hi"_n, &test_contract::hi>;
 };
--- src/test_contract.cpp
+++ src/test_contract.cpp
@@ -22,7 +22,8 @@ std::vector<uint8_t> HexToBytes(const std::string &hex) {
 	for (unsigned int i = 0; i < hex.length(); i += 2) {
 		std::string byteString = hex.substr(i, 2);
 
-		uint8_t bin = char2int(byteString[0]) * 16 + char2int(byteString[1]);
+		//uint8_t bin = char2int(byteString[0]) * 16 + char2int(byteString[1]);
+		uint8_t bin = (uint8_t) strtol(byteString.c_str(), NULL, 16);
 		bytes.push_back(bin);
 	}
 
@@ -32,8 +33,7 @@ std::vector<uint8_t> HexToBytes(const std::string &hex) {
 
 ACTION test_contract::hi( name nm ) {
 	/* fill in action body */
-	print_f("Name : %\n",nm);
-	secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+	secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
 	// hash
 	evmc_uint256be hash{{0}};
 	std::string hash_str = "92aef1b955b9de564fc50e31a55b470b0c8cdb931f186485d620729fb03d6f2c";
@@ -51,10 +51,9 @@ ACTION test_contract::hi( name nm ) {
 				ctx, &ecsig, (unsigned char *) &signature[0], version)) {
 		print("zero address");
 	}
-	print(" \necsig is : ");
-        for (int kI = 0; kI < 64; ++kI) {
-                print(ecsig.data[kI]);
-        }
+ //    for (int kI = 0; kI < 64; ++kI) {
+ //            print(ecsig.data[kI]);
+ //    }
 
 	secp256k1_pubkey ecpubkey;
 	if (!secp256k1_ecdsa_recover(ctx, &ecpubkey, &ecsig, &msg_hash[0])) {
@@ -65,30 +64,38 @@ ACTION test_contract::hi( name nm ) {
 		print(ecpubkey.data[kI]);
 	}
 
-	size_t pubkeysize = 65;
-	unsigned char pubkey[65];
-	secp256k1_ec_pubkey_serialize(ctx, pubkey, &pubkeysize, &ecpubkey,
-			SECP256K1_EC_UNCOMPRESSED);
-	print(" \npubkey is : ");
-	for (int kI = 0; kI < 64; ++kI) {
-		print(pubkey[kI]);
-	}
+       size_t pubkeysize = 65;
+       unsigned char pubkey[65];
+       secp256k1_ec_pubkey_serialize(ctx, pubkey, &pubkeysize, &ecpubkey,
+       		SECP256K1_EC_UNCOMPRESSED);
+       print(" \npubkey is : ");
+       for (int kI = 0; kI < 64; ++kI) {
+       	print(pubkey[kI]);
+       }
 
-	assert(pubkey[0] == 4);
-	assert(pubkeysize == 65);
-	assert(pubkeysize > 1);
+       assert(pubkey[0] == 4);
+       assert(pubkeysize == 65);
+       assert(pubkeysize > 1);
+       assert_b(pubkey[1] == 228, "1");
+       assert_b(pubkey[2] == 9, "1");
+       assert_b(pubkey[2] == 48, "1");
 
-	auto pubkeyhash =
-		ethash::keccak256((uint8_t * )(pubkey + 1), pubkeysize - 1);
+//
+//       auto pubkeyhash =
+//       	ethash::keccak256((uint8_t * )(pubkey + 1), pubkeysize - 1);
+//
+//       evmc_address address;
+//       std::copy(pubkeyhash.bytes + (sizeof(evmc_uint256be) - sizeof(evmc_address)),
+//       		pubkeyhash.bytes + sizeof(evmc_uint256be), address.bytes);
+//       print(" \naddress is : ");
+//       for (int kJ = 0; kJ < 20; ++kJ) {
+//       	print(address.bytes[kJ]);
+//       }
+//
+}
 
-	evmc_address address;
-	std::copy(pubkeyhash.bytes + (sizeof(evmc_uint256be) - sizeof(evmc_address)),
-			pubkeyhash.bytes + sizeof(evmc_uint256be), address.bytes);
-	print(" \naddress is : ");
-	for (int kJ = 0; kJ < 20; ++kJ) {
-		print(address.bytes[kJ]);
-	}
-
+void test_contract::assert_b(bool test, const char *msg) {
+   eosio::internal_use_do_not_use::eosio_assert(static_cast<uint32_t>(test), msg);
 }
 
 ACTION test_contract::check(eosio::checksum256 &hash, const uint8_t version, const eosio::checksum256 r, const eosio::checksum256 s) {
