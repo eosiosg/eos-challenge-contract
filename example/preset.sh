9ab62cb445c81d013d7bc612933fe4598f47b30e
Frank-AFN <a870066721@gmail.com>
2020-04-29 21:29:00 +0800

    Change: directory

--- example/preset.sh
+++ example/preset.sh
@@ -14,13 +14,13 @@ do
     cleos -u $remote create account eosio $sa EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV -p eosio
 done
 
-cleos -u $remote set contract eosio.token ../${systemContract}/eosio.token eosio.token.wasm eosio.token.abi -p eosio.token
+cleos -u $remote set contract eosio.token ${systemContract}/eosio.token eosio.token.wasm eosio.token.abi -p eosio.token
 
 cleos -u $remote push action eosio.token create '["eosio", "10000000000.0000 EOS", 0, 0, 0]' -p eosio.token
 cleos -u $remote push action eosio.token issue '["eosio", "1000000000.0000 EOS", "issue 1B to eosio"]' -p eosio
 
 # deploy system contract
-cleos -u $remote set contract eosio ../${systemContract}/eosio.system eosio.system.wasm eosio.system.abi -p eosio
+cleos -u $remote set contract eosio ${systemContract}/eosio.system eosio.system.wasm eosio.system.abi -p eosio
 
 cleos -u ${remote} system newaccount eosio eosevm111111 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 --stake-cpu "1000 EOS" --stake-net "1000 EOS" --buy-ram "1000 EOS" -p eosio
 cleos -u ${remote} transfer eosio ${contract} "1000.0000 EOS" -p eosio
