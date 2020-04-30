86efee162f42ac7e9d780ce4b1001c4ecfc4023c
Frank-AFN <a870066721@gmail.com>
2020-04-30 16:55:19 +0800

    Change set contract script

--- example/ERC20.sh
+++ example/ERC20.sh
@@ -2,6 +2,21 @@
 
 set -v
 
+cleos -u ${remote} set contract ${contract}  ../${buildfolder}/eos_evm -p ${contract}
+
+cleos -u ${remote} push action ${contract} linktoken '[{"sym":"4,EOS", "contract":"eosio.token"}]' -p ${contract}
+cleos -u $remote push action eosio updateauth '['"${contract}"',"active","owner",
+{"threshold":1,"keys":[{"key":"EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62","weight":1}],
+"waits":[],"accounts":[{"weight":1,"permission":{"actor":'"${contract}"',"permission":"eosio.code"}}]}]' -p ${contract}
+
+# create account
+cleos -u ${remote} push action ${contract} create '['"${accountb}"', "aaaaaa"]' -p ${accountb}
+cleos -u ${remote} push action ${contract} create '['"${accountb}"', d81f4358cb8cab53d005e7f47c7ba3f5116000a6]' -p ${accountb}
+cleos -u ${remote} push action ${contract} create '['"${accountc}"', "aaaaaa"]' -p ${accountc}
+cleos -u ${remote} push action ${contract} create '['"${accountc}"', 39944247c2edf660d86d57764b58d83b8eee9014]' -p ${accountc}
+cleos -u ${remote} push action ${contract} create '['"${accountd}"', "aaaaaa"]' -p ${accountd}
+cleos -u ${remote} push action ${contract} create '['"${accountd}"', e327e755438fbdf9e60891d9b752da10a38514d1]' -p ${accountd}
+
 # ERC20----------------------------------with nonce -------------------------------------
 #---create ERC20 contract [total, name, precision, symbol][10000, "first", 4, "SYS"] on address 0xe8adb07176c578547cad1fbdf0e807197fed13d2----
 # {
--- example/preset.sh
+++ example/preset.sh
@@ -32,18 +32,3 @@ cleos -u ${remote} transfer eosio ${accountc} "1000.0000 EOS" -p eosio
 cleos -u ${remote} system newaccount eosio eosevm11111d EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 --stake-cpu "1000 EOS" --stake-net "1000 EOS" --buy-ram "1000 EOS" -p eosio
 cleos -u ${remote} transfer eosio ${accountd} "1000.0000 EOS" -p eosio
 
-
-cleos -u ${remote} set contract ${contract}  ../${buildfolder}/eos_evm -p ${contract}
-
-cleos -u ${remote} push action ${contract} linktoken '[{"sym":"4,EOS", "contract":"eosio.token"}]' -p ${contract}
-cleos -u $remote push action eosio updateauth '['"${contract}"',"active","owner",
-{"threshold":1,"keys":[{"key":"EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62","weight":1}],
-"waits":[],"accounts":[{"weight":1,"permission":{"actor":'"${contract}"',"permission":"eosio.code"}}]}]' -p ${contract}
-
-# create account
-cleos -u ${remote} push action ${contract} create '['"${accountb}"', "aaaaaa"]' -p ${accountb}
-cleos -u ${remote} push action ${contract} create '['"${accountb}"', d81f4358cb8cab53d005e7f47c7ba3f5116000a6]' -p ${accountb}
-cleos -u ${remote} push action ${contract} create '['"${accountc}"', "aaaaaa"]' -p ${accountc}
-cleos -u ${remote} push action ${contract} create '['"${accountc}"', 39944247c2edf660d86d57764b58d83b8eee9014]' -p ${accountc}
-cleos -u ${remote} push action ${contract} create '['"${accountd}"', "aaaaaa"]' -p ${accountd}
-cleos -u ${remote} push action ${contract} create '['"${accountd}"', e327e755438fbdf9e60891d9b752da10a38514d1]' -p ${accountd}
