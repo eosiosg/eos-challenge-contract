82c8de1f95201ba4b9449e3d73da9b3641b73a40
Frank-AFN <a870066721@gmail.com>
2020-05-01 13:23:01 +0800

    Change set contract script (#54)
    * Change set contract script
    
    * Change: symbol EOS to SYS

--- example/preset.sh
+++ example/preset.sh
@@ -19,6 +19,8 @@ cleos -u $remote set contract eosio.token ${systemContract}/eosio.token eosio.to
 cleos -u $remote push action eosio.token create '["eosio", "10000000000.0000 EOS", 0, 0, 0]' -p eosio.token
 cleos -u $remote push action eosio.token issue '["eosio", "1000000000.0000 EOS", "issue 1B to eosio"]' -p eosio
 
+cleos -u $remote push action eosio.token create '["eosio", "10000000000.0000 SYS", 0, 0, 0]' -p eosio.token
+cleos -u $remote push action eosio.token issue '["eosio", "1000000000.0000 SYS", "issue 1B to eosio"]' -p eosio
 # deploy system contract
 cleos -u $remote set contract eosio ${systemContract}/eosio.system eosio.system.wasm eosio.system.abi -p eosio
 
@@ -27,23 +29,10 @@ cleos -u ${remote} system newaccount eosio eosevm111111 EOS7bSJkk2bC3nn7ME2Xv2Uf
 cleos -u ${remote} transfer eosio ${contract} "1000.0000 EOS" -p eosio
 cleos -u ${remote} system newaccount eosio eosevm11111b EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 --stake-cpu "1000 EOS" --stake-net "1000 EOS" --buy-ram "1000 EOS" -p eosio
 cleos -u ${remote} transfer eosio ${accountb} "1000.0000 EOS" -p eosio
+cleos -u ${remote} transfer eosio ${accountb} "1000.0000 SYS" -p eosio
 cleos -u ${remote} system newaccount eosio eosevm11111c EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 --stake-cpu "1000 EOS" --stake-net "1000 EOS" --buy-ram "1000 EOS" -p eosio
 cleos -u ${remote} transfer eosio ${accountc} "1000.0000 EOS" -p eosio
+cleos -u ${remote} transfer eosio ${accountc} "1000.0000 SYS" -p eosio
 cleos -u ${remote} system newaccount eosio eosevm11111d EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62 --stake-cpu "1000 EOS" --stake-net "1000 EOS" --buy-ram "1000 EOS" -p eosio
 cleos -u ${remote} transfer eosio ${accountd} "1000.0000 EOS" -p eosio
-
-
-cleos -u ${remote} set contract ${contract}  ../${buildfolder}/eos_evm -p ${contract}
-
-cleos -u ${remote} push action ${contract} linktoken '[{"sym":"4,EOS", "contract":"eosio.token"}]' -p ${contract}
-cleos -u $remote push action eosio updateauth '['"${contract}"',"active","owner",
-{"threshold":1,"keys":[{"key":"EOS7bSJkk2bC3nn7ME2Xv2Uf6ossmQU1zKvmvwmTc7gaH8ZcGGy62","weight":1}],
-"waits":[],"accounts":[{"weight":1,"permission":{"actor":'"${contract}"',"permission":"eosio.code"}}]}]' -p ${contract}
-
-# create account
-cleos -u ${remote} push action ${contract} create '['"${accountb}"', "aaaaaa"]' -p ${accountb}
-cleos -u ${remote} push action ${contract} create '['"${accountb}"', d81f4358cb8cab53d005e7f47c7ba3f5116000a6]' -p ${accountb}
-cleos -u ${remote} push action ${contract} create '['"${accountc}"', "aaaaaa"]' -p ${accountc}
-cleos -u ${remote} push action ${contract} create '['"${accountc}"', 39944247c2edf660d86d57764b58d83b8eee9014]' -p ${accountc}
-cleos -u ${remote} push action ${contract} create '['"${accountd}"', "aaaaaa"]' -p ${accountd}
-cleos -u ${remote} push action ${contract} create '['"${accountd}"', e327e755438fbdf9e60891d9b752da10a38514d1]' -p ${accountd}
+cleos -u ${remote} transfer eosio ${accountd} "1000.0000 SYS" -p eosio
